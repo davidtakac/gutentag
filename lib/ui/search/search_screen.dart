681c7102fa9c01d43f45ac50a6bbbdb8a6171a10
davidtakac <35954251+davidtakac@users.noreply.github.com>
2023-08-08 10:55:06 +0200

    Refactor search_screen from feedback of chatgpt

--- lib/ui/search/search_screen.dart
+++ lib/ui/search/search_screen.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gutentag/data/api_service.dart';
-import 'package:gutentag/domain/use_case/search_use_case.dart';
 import 'package:gutentag/domain/sort.dart';
-import 'package:gutentag/ui/search/search_view_model.dart';
+import 'package:gutentag/domain/use_case/search_use_case.dart';
 import 'package:gutentag/ui/common/book_card.dart';
+import 'package:gutentag/ui/common/book_card_state.dart';
 import 'package:gutentag/ui/search/filter_screen.dart';
+import 'package:gutentag/ui/search/search_view_model.dart';
 
-class BookSearchScreen extends StatelessWidget {
-  BookSearchScreen({super.key}) {
-    textController.text = viewModel.query;
+class BookSearchScreen extends StatefulWidget {
+  const BookSearchScreen({super.key});
+
+  @override
+  State<BookSearchScreen> createState() => _BookSearchScreenState();
+}
+
+class _BookSearchScreenState extends State<BookSearchScreen> {
+  late SearchViewModel viewModel;
+  late TextEditingController textController;
+  late ScrollController scrollController;
+
+  @override
+  void initState() {
+    viewModel = SearchViewModel(
+        searchBooksUseCase: SearchUseCase(apiService: ApiService()));
+    textController = TextEditingController()..text = viewModel.query;
+    scrollController = ScrollController()
+      ..addListener(() {
+        if (scrollController.position.atEdge &&
+            scrollController.position.pixels != 0) {
+          viewModel.loadNextPage();
+        }
+      });
+    super.initState();
   }
 
-  final viewModel = SearchViewModel(searchBooksUseCase: SearchUseCase(apiService: ApiService()));
-  final textController = TextEditingController();
+  @override
+  void dispose() {
+    textController.dispose();
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textStyle = theme.textTheme.titleLarge;
-    final listController = ScrollController();
-    listController.addListener(() {
-      if (listController.position.atEdge && listController.position.pixels != 0) {
-        viewModel.loadNextPage();
-      }
-    });
-
     return Scaffold(
       appBar: AppBar(
         title: TextField(
@@ -41,15 +61,15 @@ class BookSearchScreen extends StatelessWidget {
           onChanged: (query) => viewModel.query = query,
         ),
         bottom: PreferredSize(
-            preferredSize: const Size(0, 6),
-            child: ValueListenableBuilder(
-              valueListenable: viewModel.isLoading,
-              builder: (BuildContext context, bool isLoading, Widget? child) {
-                return isLoading
-                    ? const LinearProgressIndicator()
-                    : const SizedBox.shrink();
-              },
-            ),
+          preferredSize: const Size(0, 6),
+          child: ValueListenableBuilder(
+            valueListenable: viewModel.isLoading,
+            builder: (BuildContext context, bool isLoading, Widget? child) {
+              return isLoading
+                  ? const LinearProgressIndicator()
+                  : const SizedBox.shrink();
+            },
+          ),
         ),
         actions: [
           ValueListenableBuilder(
@@ -66,54 +86,43 @@ class BookSearchScreen extends StatelessWidget {
           IconButton(
             onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return FilterScreen(
-                  viewModel: viewModel,
-                );
+                return FilterScreen(viewModel: viewModel);
               }));
             },
             icon: const Icon(Icons.tune),
           ),
         ],
       ),
-      body: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: [
-          Expanded(
-            child: ValueListenableBuilder(
-                valueListenable: viewModel.results,
-                builder: (context, value, child) {
-                  return ListView(
-                    controller: listController,
-                    children: value == null || value.isEmpty
-                        ? [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
-                        child: Text(value == null
-                            ? AppLocalizations.of(context)!.search_idle
-                            : AppLocalizations.of(context)!.search_no_results(textController.text),
-                          style: theme.textTheme.bodyLarge?.copyWith(color: theme.hintColor),
-                        ),
-                      )
-                    ]
-                        : value
-                        .asMap()
-                        .map((idx, e) => MapEntry(
-                      idx,
-                      Padding(
-                          padding: EdgeInsets.fromLTRB(
-                              16,
-                              idx == 0 ? 16 : 8,
-                              16,
-                              idx == value.length - 1 ? 16 : 0),
-                          child: BookCard(cardState: value[idx])),
-                    ))
-                        .values
-                        .toList(),
-                  );
-                }),
-          ),
-        ],
-      ),
+      body: ValueListenableBuilder(
+          valueListenable: viewModel.results,
+          builder: (context, List<BookCardState>? value, child) {
+            if (value == null || value.isEmpty) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
+                child: Text(
+                  value == null
+                      ? AppLocalizations.of(context)!.search_idle
+                      : AppLocalizations.of(context)!.search_no_results(textController.text),
+                  style: theme.textTheme.bodyLarge?.copyWith(color: theme.hintColor),
+                ),
+              );
+            }
+
+            return ListView.builder(
+              controller: scrollController,
+              itemCount: value.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      16,
+                      index == 0 ? 16 : 8,
+                      16,
+                      index == value.length - 1 ? 16 : 0),
+                  child: BookCard(cardState: value[index]),
+                );
+              },
+            );
+          }),
     );
   }
 }
@@ -122,7 +131,8 @@ class SortOptionMenuButton extends StatelessWidget {
   final Sort option;
   final Function(Sort) onOptionSelected;
 
-  const SortOptionMenuButton({required this.option, required this.onOptionSelected, super.key});
+  const SortOptionMenuButton(
+      {required this.option, required this.onOptionSelected, super.key});
 
   @override
   Widget build(BuildContext context) {
